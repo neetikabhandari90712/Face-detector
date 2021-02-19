ceea0fc024409a41564192cb04d9900951aea3ad
Neetika Bhandari <68466682+neetikabhandari90712@users.noreply.github.com>
2021-02-19 14:08:47 +0530

    Update code.py

--- code.py
+++ code.py
@@ -10,12 +10,18 @@ webcam = cv2.VideoCapture(0)
 
 while True:
     sucessful_frame_read, frame = webcam.read()
+    
       # must convert to grayscale
     grayscaled_img = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+    
     # print(face_coordinates)
     for (x,y,w,h) in face_coordinates:
         cv2.rectangle(frame,(x, y),(x+w,y+h),(randrange(256),randrange(256),randrange(256)),5)
     cv2.imshow('Made by a smart programmer',frame)
     cv2.waitKey(1)
+    
+    #Stop if Q key is pressed
+    if key==81 or key == 113:
+        break
 
